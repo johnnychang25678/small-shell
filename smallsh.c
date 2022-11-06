18f61b4dae80c7778b6e19a219009a8d5ce4e992
johnnychang25678 <zjps3407@gmail.com>
2022-11-06 06:23:36 -0800

    feat: add cd, exit and foreground cmd

--- smallsh.c
+++ smallsh.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define PARSE_DELIMITER " "
 
@@ -24,47 +25,131 @@ void replaceString(char* input, char* replaceLocation,
 }
 
 
-struct parseResult {
+typedef struct parseResult {
+  int argCount; // the total arg count, include the command
   char *command;
   char* *args;
-};
+} parseResult;
 
-void parseInput(char* input, ssize_t length, struct parseResult *result){
+parseResult* parseInput(char* input){
   // split input by space (strok)
+  parseResult* result = malloc(4096);
   const char* expandSymbol = "$$";
   const int expSymbolLen = strlen(expandSymbol);
 
-  char inputWithoutNewLine[length];
-  strncpy(inputWithoutNewLine, input, length);
+  // remove the trailing new line character
+  input[strcspn(input, "\n")] = 0;
 
   int counter = 0;
-  char* *ar;
-  char *token = strtok(inputWithoutNewLine, PARSE_DELIMITER);
+  char **args = malloc(sizeof(char*)); // [char*, char*, ...]
+  char *token = strtok(input, PARSE_DELIMITER);
   while (token) {
     char *needle = strstr(token, "$$");
     if (needle != NULL) {
+      // replace $$ with pid string
       pid_t pid = getpid();
-      printf("pid: %d\n", pid);
       char pidString[10];
       sprintf(pidString, "%d", pid);
       int outLen = strlen(token) - expSymbolLen + strlen(pidString) + 1;
       char output[outLen];
 
       replaceString(token, needle, expSymbolLen, pidString, strlen(pidString), output);
-      printf("%s\n", output);
-      token = output;
+      token = malloc(sizeof(char) * outLen);
+      strcpy(token, output);
     }
     
     if (counter == 0) {
-       result->command = token; 
+       result->command = malloc(sizeof(char) * (strlen(token) + 1));
+       strcpy(result->command, token);
     } else {
-      // TODO
+      if (counter >= 1) {
+        args = realloc(args, counter * sizeof(char*)); 
+      }
+      args[counter - 1] = malloc(sizeof(char) * (strlen(token) + 1));
+      strcpy(args[counter-1], token);
+      // printf("index: %d, element: %s\n", counter - 1, token);
+      
     }
-
+    
+    counter++;
+    if (needle != NULL) free(token); // free if we have called replaceString for $$
     token = strtok(NULL, PARSE_DELIMITER);
   }
-  // TODO, scan output to a struct so we can do other stuff based on command and args
-  printf("%s\n", result->command);
+  result->argCount = counter; 
+  result->args = args;
+
+  printf("result.command %s\n", result->command);
+  int i;
+  for (i = 0; i < counter - 1; i++) {
+    printf("index: %d, arg: %s\n", i, result->args[i]);
+  }
+
+  return result;
+}
+
+void handleBuiltInCommand(parseResult* result) {
+  char* command = result->command;
+  // handle exit
+  if (strcmp(command, "exit") == 0) {
+    exit(0);
+  }
+  // handle cd
+  if (strcmp(command, "cd") == 0) {
+    char* path = result->args[0];
+    if (path == NULL) {
+      path = getenv("HOME");
+    }
+    chdir(path);
+    char buf[4096];
+    getcwd(buf, 4096);
+    printf("current path: %s\n", buf);
+  }
+  // TODO: handle status
+}
+
+void handleOtherCommand(parseResult* result) {
+  char* command = result->command;
+  // fork a child
+  pid_t spawnPid = fork();
+  int childStatus;
+
+  int argCount = result->argCount;
+  char **argv = malloc(sizeof(char*) * (argCount + 1)); // +1 is for NULL
+
+  switch(spawnPid){
+  case -1: {
+    perror("fork error\n");
+    exit(1);
+    break;
+  }
+  case 0: {
+    // in child process
+    char* cmd;
+    asprintf(&cmd, "%s%s", "/bin/", command);
+    // an array of {cmd, ...args, NULL}
+    argv[0] = cmd;
+    if (argCount > 1) {
+      int i;
+      for (i = 0; i < argCount; i++){
+        if (i == argCount - 1) {
+          argv[i+1] = NULL; 
+        } else {
+          argv[i+1] = result->args[i];
+        }
+      }
+    }
+
+    execv(argv[0], argv);
+    perror("execv error\n");
+    exit(1);
+    break;
+  }
+  default:
+    // in parent process, wait for child termination
+    waitpid(spawnPid, &childStatus, 0);
+    printf("after wait...\n");
+    free(argv);
+  }
 }
 
 int main(){
@@ -73,11 +158,31 @@ int main(){
     fflush(stdout);
     char *line = NULL;
     size_t len = 0;
-    ssize_t lineSize;
 
-    struct parseResult result;
-    lineSize = getline(&line, &len, stdin);
-    parseInput(line, lineSize - 1, &result);
+    // parseResult result;
+    getline(&line, &len, stdin);
+    if (line[0] != ':') continue;
+    if (line[1] != ' ') continue;
+    char *lineCopy;
+    lineCopy = line + 2; 
+    parseResult* result = NULL;
+    result = parseInput(lineCopy);
+    // TODO: do stuff with commands and args
+    char* command = result->command;
+    if (strcmp(command, "exit") == 0 || strcmp(command, "cd") == 0 || strcmp(command, "status") == 0) {
+      handleBuiltInCommand(result);
+    }
+
+    // handle non-builtin
+    handleOtherCommand(result);
+
+
+    // clean up the memory
+    free(result->command);
+    for (int i = 0; i < result->argCount - 1; i++) {
+      free(result->args[i]);
+    }
+    free(result);
     free(line);
   }
 }
